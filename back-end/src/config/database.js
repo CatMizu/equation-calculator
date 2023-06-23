700c33d9d6b047620245ab4787c0031d6b0cc4a1
CatMizu <zhrmghgjht@gmail.com>
2023-06-23 14:31:35 -0400

    modify readme

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -2,15 +2,10 @@ const { Sequelize } = require('sequelize');
 const logger = require('./logger');
 const config = require('./config');
 
-const sequelize = new Sequelize(
-  config.mysql.database, 
-  config.mysql.username, 
-  config.mysql.password, 
-  {
-    dialect: config.mysql.dialect,
-    host: config.mysql.host,
-  }
-);
+const sequelize = new Sequelize(config.mysql.database, config.mysql.username, config.mysql.password, {
+  dialect: config.mysql.dialect,
+  host: config.mysql.host,
+});
 
 sequelize.sync().then(() => logger.info('Tables have been created'));
 
